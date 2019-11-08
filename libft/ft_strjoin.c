2bc7fbe1a31a37a2416cc8aad8905d6fdfae96e9
Thomas DE PHUOC <tde-phuo@e2r4p10.42.fr>
2019-11-08 14:45:16 +0100

    edit

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -1,33 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tde-phuo <tde-phuo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/08 12:06:10 by tde-phuo          #+#    #+#             */
-/*   Updated: 2019/11/08 13:45:55 by tde-phuo         ###   ########.fr       */
+/*   Created: 2019/11/08 13:46:54 by tde-phuo          #+#    #+#             */
+/*   Updated: 2019/11/08 14:36:30 by tde-phuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*ptr;
+	size_t	len_s1;
+	size_t	len_s2;
 	size_t	i;
+	char	*ptr;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
 	i = 0;
-	if (s == NULL)
+	if (!(ptr = malloc(sizeof(char) * (len_s1 + len_s2 + 1))))
 		return (NULL);
-	if (ft_strlen(s) < start)
-		return (ft_strdup(""));
-	if (!(ptr = malloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	while (i < len)
+	while (i < len_s1)
 	{
-		ptr[i] = (char)s[start + i];
-		++i;
+		ptr[i] = *s1++;
+		i++;
+	}
+	while (i < len_s1 + len_s2)
+	{
+		ptr[i] = *s2++;
+		i++;
 	}
 	ptr[i] = '\0';
 	return (ptr);
