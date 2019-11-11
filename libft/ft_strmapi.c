9b328a19b30f935f7b78fffa283d495d1ef00bb8
Thomas DE PHUOC <tde-phuo@e2r5p8.42.fr>
2019-11-11 17:14:37 +0100

    fd functions

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tde-phuo <tde-phuo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/11 16:10:52 by tde-phuo          #+#    #+#             */
+/*   Updated: 2019/11/11 16:23:43 by tde-phuo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char		*ft_strnull(void)
+{
+	char *s;
+
+	s = malloc(sizeof(char *));
+	s[0] = 0;
+	return (s);
+}
+
+char			*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	length;
+	size_t	i;
+	char	*s2;
+
+	if (!s || s[0] == '\0')
+		return (ft_strnull());
+	i = 0;
+	length = ft_strlen(s);
+	if (!(s2 = malloc(sizeof(char) * (length + 1))))
+		return (NULL);
+	while (i < length)
+	{
+		s2[i] = (*f)(i, s[i]);
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: schlesinger <schlesinger@student.42.fr>    +#+  +:+       +#+        */
+/*   By: tde-phuo <tde-phuo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 14:20:10 by tde-phuo          #+#    #+#             */
-/*   Updated: 2019/11/10 20:50:33 by schlesinger      ###   ########.fr       */
+/*   Updated: 2019/11/11 17:08:29 by tde-phuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,10 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
